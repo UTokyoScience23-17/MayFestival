94b2ee602cafeeddab59e34202b790596ea20b97
tou-rou <86340335+tou-rou@users.noreply.github.com>
2024-04-26 15:16:25 +0900

    changed script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 let imgs = document.getElementById("pics").querySelectorAll("img");
 for(let i =0; i<imgs.length;i++){
-    imgs[i].style.animationDelay=String(10*i-21)+'s'
+    imgs[i].style.animationDelay=String(10*i-31)+'s'
 }
